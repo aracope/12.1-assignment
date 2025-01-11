54306e2aae28e28b9dbef14f30c703bdb8f6db59
aracope <156398622+aracope@users.noreply.github.com>
2025-01-10 18:39:37 -0800

    Successful Jasmine test of extractKey map function.

--- foreach-map-filter.js
+++ foreach-map-filter.js
@@ -134,8 +134,11 @@ Examples:
 */
 
 function extractKey(arr, key) {
-
+  return arr.map(function(val) {
+    return val[key];
+  });
 }
+console.log(extractKey([{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}], 'name'));
 
 /*
 Write a function called extractFullName which accepts an array of vowelCountects and returns a new array with the value of the key with a name of "first" and the value of a key with the name of  "last" in each vowelCountect, concatenated together with a space. 
